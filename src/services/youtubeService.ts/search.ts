fea3d70dd20a330cf9a66a09d4b6b1f012dc3126
krsna <yogikkyadav@gmail.com>
2023-07-25 17:22:25 +0530

    added resource of api

--- src/services/youtubeService.ts/search.ts
+++ src/services/youtubeService.ts/search.ts
@@ -1,5 +1,6 @@
 import { API_KEY, BASE_URL } from './constants'
 
+//https://developers.google.com/youtube/v3/docs/search/list
 const searchByKeyword = (keyword: string): any => {
         fetch(`${BASE_URL}/search?part=snippet&maxResults=25&q=${keyword}&key=${API_KEY}`)
 }
