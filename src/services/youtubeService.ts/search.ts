36312165b764f202d0497e77029ed47f911bbed1
krsna <yogikkyadav@gmail.com>
2023-07-25 17:33:54 +0530

    added requirements

--- src/services/youtubeService.ts/search.ts
+++ src/services/youtubeService.ts/search.ts
@@ -1,7 +1,15 @@
 import { API_KEY, BASE_URL } from './constants'
 
-//https://developers.google.com/youtube/v3/docs/search/list
+
+// things to implement
+// 1. Search
+// 2. Debounced Search
+// 3. Fetching next page with token
+// 4. Infinite Scroll
+// 5. order
+// 6. filters
 const searchByKeyword = (keyword: string): any => {
+        //Resource: https://developers.google.com/youtube/v3/docs/search/list
         fetch(`${BASE_URL}/search?part=snippet&maxResults=25&q=${keyword}&key=${API_KEY}`)
 }
 
